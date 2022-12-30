5e43c006516a64cef93752711b49f9341d9938e3
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-12-30 03:02:25 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,17 @@ import { Box } from 'components/Box';
 const Home = () => {
   return (
     <main>
-      <Box p={[4]} as="section">
-        <h1>PHONEBOOK</h1>
+      <Box py={[4]} as="section">
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          px={[4]}
+          mx="auto"
+          maxWidth={1400}
+          as="div"
+        >
+          <h1>PHONEBOOK</h1>
+        </Box>
       </Box>
     </main>
   );
